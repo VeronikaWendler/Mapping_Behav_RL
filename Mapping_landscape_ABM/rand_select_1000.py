ee9a0ccb92c0cb1ab294ddff3087e44be6475b39
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-07 22:12:37 +0000

    randomly selected 100 papers to label their abstracts

--- Mapping_landscape_ABM/rand_select_1000.py
+++ Mapping_landscape_ABM/rand_select_1000.py
@@ -1,2 +1,16 @@
-# code to randomly select 1000 papers from the Scopus file
+# code to randomly select 100 papers from the Scopus file
 
+import pandas as pd
+from pathlib import Path
+
+in_path = Path(r"D:\Birmingham_Uni_Oct25\Papers\Mapping_Behav_RL\Mapping_landscape_ABM\Data\data_cleaned.csv")
+out_path = in_path.parent / "data_rand_100_labels.csv"
+df = pd.read_csv(in_path)
+
+cols = ["id", "Abstract_cleaned"]
+
+n = 100
+
+df_sample = df.sample(n=n, replace=False, random_state=None)[cols]
+df_sample.to_csv(out_path, index=False)
+print(f"Saved {len(df_sample)} rows to: {out_path}")
