5da6615171d85599e351016a0009a97cfff9c2a5
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-11 22:19:17 +0000

    abstract error

--- run_files.sh
+++ run_files.sh
@@ -18,8 +18,10 @@ module load R/4.5.0-gfbf-2024a
 source ~/apps/miniforge3/etc/profile.d/conda.sh
 conda activate mapping_abm
 
+
 export HF_HOME=$HOME/.cache/huggingface
-export TRANSFORMERS_CACHE=$HOME/.cache/huggingface
+export HF_HUB_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
 export MPLCONFIGDIR=${SLURM_TMPDIR:-/tmp}/mplcache
 export PYTHONUNBUFFERED=1
 export TOKENIZERS_PARALLELISM=false
