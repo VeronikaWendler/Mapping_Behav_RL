1936fcf45651e8ebc4fbd815169897c29e38ce25
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-11 16:48:41 +0000

    get data onto the cluster

---
+++ Mapping_landscape_ABM/Data/interesting_IDs.py
@@ -0,0 +1,5 @@
+# id 7274: talks about bm in epidemic modelling and is history-based, could be used for looking at the history of ABMs in social science through the lense of such a model, e.g. modleling emergance of particular fields
+# id 194 : Spread of missinformation on social media
+# id 3725: Cooperation vs Competition abm simulation
+# id 4887: Grounded theory + ABM ...qualitative research + ABM 
+# id 1330:social psychology ABM
