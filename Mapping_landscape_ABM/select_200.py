e4e0b143bfed324686a41679b4cc195e0912a7ce
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-15 20:09:13 +0000

    selecting only 200 labels

---
+++ Mapping_landscape_ABM/select_200.py
@@ -0,0 +1,13 @@
+# selecting the first 200 rows of the dataframe
+
+import pandas as pd 
+from pathlib import Path
+
+
+in_path = Path(r"D:\Birmingham_Uni_Oct25\Papers\Mapping_Behav_RL\Mapping_landscape_ABM\Data\data_selected_labels_4.csv")
+out_new = in_path.parent / "data_selected_labels_4_200.csv"
+
+df = pd.read_csv(in_path)
+df_200 = df.head(201)
+df_200.to_csv(out_new, index=True)
+
