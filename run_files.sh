f99283b488c5d5dd9aa933b0f96fcb0980df8866
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-19 00:28:57 +0000

    more mem

--- run_files.sh
+++ run_files.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=2.1_semantic_ratings                      # should be changed to whatever you are running, otherwise confusion 
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=6
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --error=logs/%x.%j.err
 #SBATCH --mail-type=ALL
