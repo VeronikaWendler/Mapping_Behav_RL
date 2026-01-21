224131c1352183a9eed2149d8544866298624819
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-21 18:15:11 +0000

    resolving cash issue

--- run_files.sh
+++ run_files.sh
@@ -20,17 +20,28 @@ cd ~/projects/Mapping_Behav_RL
 source ~/apps/miniforge3/etc/profile.d/conda.sh
 conda activate mapping_abm
 
-# make conda libs preferred, then force conda OpenSSL into the process
+export HF_HOME=/rds/homes/v/vaw508/projects/Mapping_Behav_RL/hf_cache
+export HF_HUB_CACHE=$HF_HOME/hub
+export TRANSFORMERS_CACHE=$HF_HOME/transformers
+export TORCH_HOME=$HF_HOME/torch
+mkdir -p "$HF_HUB_CACHE" "$TRANSFORMERS_CACHE" "$TORCH_HOME"
+
+# force conda OpenSSL into the process
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 export LD_PRELOAD="$CONDA_PREFIX/lib/libcrypto.so.3:$CONDA_PREFIX/lib/libssl.so.3"
 
-export HF_HOME=$HOME/.cache/huggingface
+# allow downloading on first run
 export HF_HUB_OFFLINE=0
 export TRANSFORMERS_OFFLINE=0
+
 export MPLCONFIGDIR=${SLURM_TMPDIR:-/tmp}/mplcache
 export PYTHONUNBUFFERED=1
 export TOKENIZERS_PARALLELISM=false
 
-Rscript Mapping_landscape_ABM/3_taxonomy.R
+# just temporarily, to check
+echo "HF_HOME=$HF_HOME"
+echo "HF_HUB_CACHE=$HF_HUB_CACHE"
+echo "TRANSFORMERS_CACHE=$TRANSFORMERS_CACHE"
 
+Rscript Mapping_landscape_ABM/3_taxonomy.R
 
