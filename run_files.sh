ccce9dc4d5c5f275b8aaa459addd4930ba71faef
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-21 18:24:28 +0000

    resolving cash issue nr 2

--- run_files.sh
+++ run_files.sh
@@ -20,28 +20,30 @@ cd ~/projects/Mapping_Behav_RL
 source ~/apps/miniforge3/etc/profile.d/conda.sh
 conda activate mapping_abm
 
+# HuggingFace cache
 export HF_HOME=/rds/homes/v/vaw508/projects/Mapping_Behav_RL/hf_cache
-export HF_HUB_CACHE=$HF_HOME/hub
-export TRANSFORMERS_CACHE=$HF_HOME/transformers
+export HF_DATASETS_CACHE=$HF_HOME/datasets
 export TORCH_HOME=$HF_HOME/torch
-mkdir -p "$HF_HUB_CACHE" "$TRANSFORMERS_CACHE" "$TORCH_HOME"
 
-# force conda OpenSSL into the process
+mkdir -p "$HF_HOME" "$HF_DATASETS_CACHE" "$TORCH_HOME"
+
+unset TRANSFORMERS_CACHE
+unset HF_HUB_CACHE
+
+# temp files 
+export TMPDIR=/rds/homes/v/vaw508/projects/Mapping_Behav_RL/tmp
+mkdir -p "$TMPDIR"
+
+# OpenSSL
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 export LD_PRELOAD="$CONDA_PREFIX/lib/libcrypto.so.3:$CONDA_PREFIX/lib/libssl.so.3"
 
-# allow downloading on first run
+# allow downloads
 export HF_HUB_OFFLINE=0
 export TRANSFORMERS_OFFLINE=0
 
-export MPLCONFIGDIR=${SLURM_TMPDIR:-/tmp}/mplcache
+export MPLCONFIGDIR=${TMPDIR}/mplcache
 export PYTHONUNBUFFERED=1
 export TOKENIZERS_PARALLELISM=false
 
-# just temporarily, to check
-echo "HF_HOME=$HF_HOME"
-echo "HF_HUB_CACHE=$HF_HUB_CACHE"
-echo "TRANSFORMERS_CACHE=$TRANSFORMERS_CACHE"
-
 Rscript Mapping_landscape_ABM/3_taxonomy.R
-
