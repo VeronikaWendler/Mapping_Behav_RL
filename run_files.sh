56465981c517b404c1a087f0a560fce05ae8a501
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-22 12:15:28 +0000

    0_read.py

--- run_files.sh
+++ run_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=2.1_semantic_ratings                      # should be changed to whatever you are running, otherwise confusion 
+#SBATCH --job-name=3_taxonomy.R
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=128G
@@ -13,22 +13,41 @@ mkdir -p logs
 
 module purge
 module load bear-apps/2024a/live
-export LD_PRELOAD="$CONDA_PREFIX/lib/libcrypto.so.3:$CONDA_PREFIX/lib/libssl.so.3"
-
+module load GCC/13.3.0
 module load R/4.5.0-gfbf-2024a
-
-export HF_HOME=$HOME/.cache/huggingface
-export HF_HUB_OFFLINE=1
-export TRANSFORMERS_OFFLINE=1
-export MPLCONFIGDIR=${SLURM_TMPDIR:-/tmp}/mplcache
-export PYTHONUNBUFFERED=1
-export TOKENIZERS_PARALLELISM=false
 
 cd ~/projects/Mapping_Behav_RL
 
 source ~/apps/miniforge3/etc/profile.d/conda.sh
 conda activate mapping_abm
-Rscript Mapping_landscape_ABM/3_taxonomy.R         # or python
+
+BB_WORKDIR=$(mktemp -d /scratch/${USER}_${SLURM_JOBID}.XXXXXX)
+export TMPDIR=${BB_WORKDIR}
+
+export HF_HOME=${BB_WORKDIR}/hf
+export HF_DATASETS_CACHE=${HF_HOME}/datasets
+export TORCH_HOME=${HF_HOME}/torch
+export XDG_CACHE_HOME=${HF_HOME}/xdg
+mkdir -p "$HF_HOME" "$HF_DATASETS_CACHE" "$TORCH_HOME" "$XDG_CACHE_HOME"
+
+unset TRANSFORMERS_CACHE
+unset HF_HUB_CACHE
+
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
+export LD_PRELOAD="$CONDA_PREFIX/lib/libcrypto.so.3:$CONDA_PREFIX/lib/libssl.so.3"
+
+export HF_HUB_OFFLINE=0
+export TRANSFORMERS_OFFLINE=0
+
+export MPLCONFIGDIR=${TMPDIR}/mplcache
+export PYTHONUNBUFFERED=1
+export TOKENIZERS_PARALLELISM=false
+
+python Mapping_landscape_ABM/0_read.py
+
+rm -rf "${BB_WORKDIR}"
+
+
 
 
 
