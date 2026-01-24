41fc82fcbd42453cd7513c6fa4eb50537238f269
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-24 12:56:52 +0000

    runnign things again for better documentation

--- run_files.sh
+++ run_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=3_taxonomy.R
+#SBATCH --job-name=2.1_semantic_ratings                      # should be changed to whatever you are running, otherwise confusion 
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=128G
@@ -13,46 +13,48 @@ mkdir -p logs
 
 module purge
 module load bear-apps/2024a/live
-module load GCC/13.3.0
+export LD_PRELOAD="$CONDA_PREFIX/lib/libcrypto.so.3:$CONDA_PREFIX/lib/libssl.so.3"
+
 module load R/4.5.0-gfbf-2024a
+
+export HF_HOME=$HOME/.cache/huggingface
+export HF_HUB_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+export MPLCONFIGDIR=${SLURM_TMPDIR:-/tmp}/mplcache
+export PYTHONUNBUFFERED=1
+export TOKENIZERS_PARALLELISM=false
 
 cd ~/projects/Mapping_Behav_RL
 
 source ~/apps/miniforge3/etc/profile.d/conda.sh
 conda activate mapping_abm
+python Mapping_landscape_ABM/0_read.py         # or python or Rscript
 
-BB_WORKDIR=$(mktemp -d /scratch/${USER}_${SLURM_JOBID}.XXXXXX)
-export TMPDIR=${BB_WORKDIR}
 
-export HF_HOME=${BB_WORKDIR}/hf
-export HF_DATASETS_CACHE=${HF_HOME}/datasets
-export TORCH_HOME=${HF_HOME}/torch
-export XDG_CACHE_HOME=${BB_WORKDIR}/xdg
-export MPLCONFIGDIR=${BB_WORKDIR}/mplcache
 
-export PIP_CACHE_DIR=${BB_WORKDIR}/pip
-export CONDA_PKGS_DIRS=${BB_WORKDIR}/conda_pkgs
-export NUMBA_CACHE_DIR=${BB_WORKDIR}/numba
-export JOBLIB_TEMP_FOLDER=${BB_WORKDIR}/joblib
 
-mkdir -p "$HF_HOME" "$HF_DATASETS_CACHE" "$TORCH_HOME" "$XDG_CACHE_HOME" \
-         "$PIP_CACHE_DIR" "$CONDA_PKGS_DIRS" "$NUMBA_CACHE_DIR" "$JOBLIB_TEMP_FOLDER" \
-         "$MPLCONFIGDIR"
 
-unset TRANSFORMERS_CACHE
-unset HF_HUB_CACHE
-export TOKENIZERS_PARALLELISM=false
-export PYTHONUNBUFFERED=1
 
-export OUTDIR=${BB_WORKDIR}/outputs
-mkdir -p "$OUTDIR"
 
-Rscript Mapping_landscape_ABM/1_clean_abstracts.R
 
-# results back to RDS
-cp -v "${OUTDIR}"/*  ~/projects/Mapping_Behav_RL/Mapping_landscape_ABM/Data/ || true
 
-rm -rf "${BB_WORKDIR}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
