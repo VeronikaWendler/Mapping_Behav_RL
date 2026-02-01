b8e13433acc1b8f3182c404f4ce734a17fbe2a6f
VeronikaWendler <wendler.vroni@gmail.com>
2026-02-01 12:57:53 +0000

    ...

--- Mapping_landscape_ABM/3_taxonomy.R
+++ Mapping_landscape_ABM/3_taxonomy.R
@@ -39,114 +39,72 @@ tags <- tags %>%
 tags_tab <- tags$tags |> unlist() |> table() |> sort(decreasing = TRUE)
 
 
-#------
-# old 
-torch = import("torch")
-st = import("sentence_transformers")
-
-device <- if (torch$cuda$is_available()) "cuda" else "cpu"
-cat("Using device:", device, "\n")
-model <- st$SentenceTransformer("Qwen/Qwen3-Embedding-4B", device = device)
-
-prompts = paste0(names(tags_tab), " in behavioral agent-based modelling")
-
-tag_emb <- model$encode(prompts, batch_size = as.integer(16), show_progress_bar = TRUE)
-rownames(tag_emb) = names(tags_tab)
-saveRDS(tag_emb, "/rds/projects/z/zhanglp-vwendler-core/ABM_Mapping/Data/embs_300/data_tags_embedding.RDS")
-#------
-
-
-
-
-# #---------
-# torch <- import("torch")
-# st <- import("sentence_transformers")
-
-# model <- st$SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device = "cpu")
-
-# prompts <- paste0(names(tags_tab), " in behavioral agent-based modelling")
-
-# chunk_size <- as.integer(500)
-# n <- length(prompts)
-
-# chunk_id <- ((seq_len(n) - 1) %/% chunk_size) + 1
-# chunks <- split(seq_len(n), chunk_id)
-
-# emb_list <- vector("list", length(chunks))
-
-# for (i in seq_along(chunks)) {
-#   cat(sprintf("Embedding chunk %d / %d\n", i, length(chunks)))
-#   emb_list[[i]] <- model$encode(
-#     prompts[chunks[[i]]],
-#     batch_size = as.integer(32),
-#     show_progress_bar = TRUE
-#   )
-# }
-
-# tag_emb <- do.call(rbind, emb_list)
-# rownames(tag_emb) <- names(tags_tab)
-
-# scratch_out <- file.path(Sys.getenv("TMPDIR"), "data_tags_embedding.RDS")
-# saveRDS(tag_emb, scratch_out)
-# final_out <- "Mapping_landscape_ABM/Data/embeddings/embs_300/data_tags_embedding.RDS"
-# ok <- file.copy(scratch_out, final_out, overwrite = TRUE)
-# if (!ok) stop("file.copy() failed: could not copy embeddings back to /rds")
-# cat("Saved embeddings to:", final_out, "\n")
-
-
-
-##########################
-# # EMBED 
+# #------
+# # old 
 # torch = import("torch")
 # st = import("sentence_transformers")
 
-# model = st$SentenceTransformer("Qwen/Qwen3-Embedding-4B", device = "cpu")
+# device <- if (torch$cuda$is_available()) "cuda" else "cpu"
+# cat("Using device:", device, "\n")
+# model <- st$SentenceTransformer("Qwen/Qwen3-Embedding-4B", device = device)
 
-# prompts = paste0(names(tags_tab), " in behavioral reinforcement learning")
+# prompts = paste0(names(tags_tab), " in behavioral agent-based modelling")
 
-# tag_emb = model$encode(prompts)
+# tag_emb <- model$encode(prompts, batch_size = as.integer(16), show_progress_bar = TRUE)
 # rownames(tag_emb) = names(tags_tab)
-# saveRDS(tag_emb, "Mapping_landscape_ABM/Data/tagging/data_tags_embedding.RDS")
-##########################
-# tag_emb = readRDS("1_data/tagging/data_tags_embedding.RDS")
+# saveRDS(tag_emb, "/rds/projects/z/zhanglp-vwendler-core/ABM_Mapping/Data/embs_300/data_tags_embedding.RDS")
 
-# tag_cos = arma_cosine(tag_emb)
-# rownames(tag_cos) = colnames(tag_cos) = rownames(tag_emb)
 
-# tag_cos[tag_cos > 1] = 1
-# tag_cos[tag_cos < -1] = -1
-# tag_cos = 1-acos(tag_cos)/pi
+#------
+# EMBED ---
 
-# eps = 1/20**10
-# cutoff = quantile(tag_cos[upper.tri(tag_cos)], .90) - eps
+tag_emb = readRDS("/rds/projects/z/zhanglp-vwendler-core/ABM_Mapping/Data/embs_300/data_tags_embedding.RDS")
+stopifnot(!is.null(rownames(tag_emb)))
 
-# tag_clust = fastcluster::hclust(as.dist(1 - tag_cos),
-#                                  method = "complete")
+tag_cos = arma_cosine(tag_emb)
+rownames(tag_cos) = colnames(tag_cos) = rownames(tag_emb)
 
-# number_steps = sum(tag_clust$height <  (1 - cutoff))
-# tag_clustering = cutree(tag_clust, k = nrow(tag_cos) - number_steps)
-# names(tag_clustering) = rownames(tag_emb)
+tag_cos[tag_cos > 1] = 1
+tag_cos[tag_cos < -1] = -1
+tag_cos = 1-acos(tag_cos)/pi
 
-# tag_cliques = split(names(tag_clustering), tag_clustering)
-# #clique_minimum = sapply(tag_cliques, function(x) tag_cos[x, x] |> min())
+eps = 1/20^10
+cutoff = quantile(tag_cos[upper.tri(tag_cos)], .90) - eps
 
-# # COMBINE ---
+tag_clust = fastcluster::hclust(as.dist(1 - tag_cos),
+                                 method = "complete")
 
-# tag_processed = 
-#   tibble(tag = unlist(tag_cliques),
-#          n = tags_tab[unlist(tag_cliques)] |> c(),
-#          label = lapply(tag_cliques, function(x) {
-#            n = tags_tab[x]
-#            rep(x[which.max(n)], length(x))
-#          }) |> unlist())
+number_steps = sum(tag_clust$height <  (1 - cutoff))
+tag_clustering = cutree(tag_clust, k = nrow(tag_cos) - number_steps)
+names(tag_clustering) = rownames(tag_emb)
 
-# tag_dict = tag_processed |> pull(label, tag)
+tag_cliques = split(names(tag_clustering), tag_clustering)
+#clique_minimum = sapply(tag_cliques, function(x) tag_cos[x, x] |> min())
+
+
+#----
+# COMBINE ---
+
+tag_processed = 
+  tibble(tag = unlist(tag_cliques),
+         n = tags_tab[unlist(tag_cliques)] |> c(),
+         label = lapply(tag_cliques, function(x) {
+           n = tags_tab[x]
+           rep(x[which.max(n)], length(x))
+         }) |> unlist())
+
+tag_dict = tag_processed |> pull(label, tag)
 
 # tags = tags |> 
 #   mutate(tags_clean = lapply(tags, function(x) tag_dict[x])) |> 
 #   rename(tags_out = out)
 
-# data = read_csv("1_data/data_cleaned_filtered.csv")
+tags <- tags |>
+  mutate(tags_clean = lapply(.data$tags, function(x) unname(tag_dict[x]))) |>
+  rename(tags_out = out)
 
-# data = data |> left_join(tags, by = "id")
-# saveRDS(data, "1_data/data_cleaned_filtered_tagged.RDS")
+
+data = read_csv("Mapping_landscape_ABM/Data/data_cleaned_filtered_4_300.csv")
+
+data = data |> left_join(tags, by = "id")
+saveRDS(data, "/rds/projects/z/zhanglp-vwendler-core/ABM_Mapping/Data/embs_300/data_cleaned_filtered_tagged.RDS")
