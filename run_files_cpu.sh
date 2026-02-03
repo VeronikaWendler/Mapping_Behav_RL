0ead560b91eafa31ef149cb220f286cfcb170461
VeronikaWendler <wendler.vroni@gmail.com>
2026-02-03 18:43:27 +0000

    time change

--- run_files_cpu.sh
+++ run_files_cpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=64G
-#SBATCH --time=00:02:00
+#SBATCH --time=02:00:00
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --error=logs/%x.%j.err
 #SBATCH --mail-type=ALL
