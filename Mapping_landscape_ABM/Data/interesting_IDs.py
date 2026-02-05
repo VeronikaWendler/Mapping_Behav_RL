b92b3d450ac1f8ade6aafaf695f6c14734c97956
Veronika Wendler (PhD Psychology Lab OS UKRI FT) <vaw508@bear-pg-login06.bear.cluster>
2026-02-05 01:03:34 +0000

    Update interesting IDs

--- Mapping_landscape_ABM/Data/interesting_IDs.py
+++ Mapping_landscape_ABM/Data/interesting_IDs.py
@@ -1,10 +1,11 @@
+
 # ID: 7274 - talks about bm in epidemic modelling and is history-based, could be used for looking at the history of ABMs in social science through the lense of such a model, e.g. modleling emergance of particular fields
 
 # ID: 194 - Spread of missinformation on social media
 
 # ID: 3725 - Cooperation vs Competition abm simulation
 
-# ID: 4887 - Grounded theory + ABM ...qualitative research + ABM 
+# ID: 4887 - Grounded theory + ABM ...qualitative research + ABM
 
 # ID: 1330 - social psychology ABM
 
@@ -19,6 +20,74 @@
 # ID: 2339 — Cars of the future; the role of user behaviour
 
 # ID: 6139 — Farmer–environment interaction (land-use change simulations from 1989 → 2080)
+
+# ID: 2667 — ABM + evolutionary algo in relative vs absolute price comparison performance
+
+# ID: 3900 — Denial of climate change: ABM + evolution of scientific & public opinion during the last 4 decades
+
+# ID: 1264 — Economic circuit theory. Use electrical circuit theory for economic systems
+
+# ID: 3989 — ABM for multi-period supply chain + risk + heuristics (compare of genetic vs Q-learning algo)
+
+# ID: 6535 — Diffusion of new products + psychology + ABM
+
+# ID: 5689 — ABMs & emotion
+
+# ID: 2434 — Twitter & ABM & misinformation
+
+# ID: 5296 — Gossip & Twitter & marketing ABM
+
+# ID: 836 — Effective Altruism ?
+
+# ID: 4335 — Aberdeen ABM
+
+# Additional notes:
+
+# ID: 571 — Individual vs team-learning ABM – autonomous vs team decision-making + RL
+
+# ID: 4337 — Agent-based comp. economics (ACE): comparison of different models & position of ABM
+
+# ID: 4336 — Biowaste + ABM ⇒ Policy outcomes depend not only on current policy levels but also on the historical path taken
+
+# ID: 166 — Agent-based social influence model → distance-based vs RL rules
+
+# ID: 8286 - Different types of ABMs in the social sciences and their respective validation strategies
+
+# ID: 2797 - OpenABM - ABM and covid19
+
+# ID: 1432 - history ABM:  social processes, including violent conflict, played a crucial role in the shaping of population dynamics of European Mid-Holocene societies
+
+# ID: 6644 - cognitive-motivational-social-environmental interaction
+
+# ID: 4061 - cross-disciplinary discussion among researchers and a goal-oriented synthesis to identify the general principles and theories essential to improve our understanding and management of complex system
+
+# ID: 5967 - social psychology & tax compliance ABM ???
+
+# ID> 8107 - Intro to ABM in Bio
+
+# ID: 8287 - Social Sciences are missing ABMs
+
+# ID: 7274 - talks about bm in epidemic modelling and is history-based, could be used for looking at the history of ABMs in social science through the lense of such a model, e.g. modleling emergance of particular fields
+
+# ID: 194 - Spread of missinformation on social media
+
+# ID: 3725 - Cooperation vs Competition abm simulation
+
+# ID: 4887 - Grounded theory + ABM ...qualitative research + ABM
+
+# ID: 1330 - social psychology ABM
+
+# ID: 6485 — Scientific communities exhibit herding behaviour when members are reluctant to take research risks
+
+# ID: 4048 — Bayesian optimization in an ABM on human–environment interaction in the context of ocean fisheries
+
+# ID: 3306 — Fuzzy agent BM on predicting crew performance (subjective + objective uncertainties)
+
+# ID: 5803 — Institutionalizing a local practice or persistence vs change in institutionalization using a cognitive + social case analysis ABM
+
+# ID: 2339 — Cars of the future; the role of user behaviour
+
+# ID: 897 — Farmer–environment interaction (land-use change simulations from 1989 → 2080)
 
 # ID: 2667 — ABM + evolutionary algo in relative vs absolute price comparison performance
 
